3c15affc00da7415f9ad18ff15a7f64ccbd18f91
mchaudhry02 <mchaudhry20@hotmail.com>
2026-08-30 19:14:11 -0500

    add teacherrepository.java

---
+++ classmanager/src/main/java/com/launchcode/classmanager/repository/TeacherRepository.java
@@ -0,0 +1,9 @@
+package com.launchcode.classmanager.repository;
+
+import com.launchcode.classmanager.model.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+}
