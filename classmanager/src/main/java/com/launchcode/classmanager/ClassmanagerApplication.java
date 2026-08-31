3ea56a55c2e8095bb2b0f793f3f96b120be62d81
mchaudhry02 <mchaudhry20@hotmail.com>
2026-08-30 19:11:11 -0500

    add ClassmanagerApplication

---
+++ classmanager/src/main/java/com/launchcode/classmanager/ClassmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.launchcode.classmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassmanagerApplication.class, args);
+    }
+
+}
